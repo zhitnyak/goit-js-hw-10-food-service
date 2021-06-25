39acfb9721f7fbcfed6ac780ad76ce10bcae802e
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-06-25 22:47:21 +0300

    themes_complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
 import './sass/main.scss';
+// import menuCards from './templates/menu-cards.hbs';
+import elMarkup from './menu.json';
+
+//Меняем тему
+const refs = {
+  body: document.querySelector('body'),
+  switch: document.querySelector('.theme-switch__toggle'),
+};
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+refs.switch.addEventListener('change', onCheckBoxChange);
+const STORAGE_KEY = 'theme';
+
+function onCheckBoxChange(evt) {
+  const checked = refs.switch.checked;
+
+  refs.body.classList.toggle(Theme.LIGHT);
+  refs.body.classList.toggle(Theme.DARK);
+
+  if (checked) {
+    localStorage.setItem('STORAGE_KEY', Theme.DARK);
+  } else {
+    localStorage.removeItem('STORAGE_KEY');
+    localStorage.setItem('STORAGE_KEY', Theme.LIGHT);
+  }
+}
+const currentTheme = localStorage.getItem('STORAGE_KEY');
+
+if (currentTheme === Theme.DARK) {
+  refs.body.classList.add(Theme.DARK);
+  refs.switch.checked = true;
+}
+
+// Делаем разметку
+const cardBox = document.querySelector('.js-menu');
+
+function createGallery(cards) {
+  return cards.map(e => menuCards(e)).join('');
+}
+
+const cardMarkup = createGallery(elMarkup);
+cardBox.insertAdjacentHTML('beforeend', cardMarkup);
